f5955e80dab3494e742ae5a711a6488892d128d8
davide-1536 <davide.franzoso98@gmail.com>
2025-01-26 12:20:57 +0100

    Neural network weights loading

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,6 @@
 import cv2
 import os
-
+import numpy as np
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
@@ -9,3 +9,62 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
+
+def find_bounding_boxes(frame, outs, objectnessThreshold=0.5, confThreshold = 0.5, nmsThreshold = 0.3, debug = True):
+    """Remove the bounding boxes with low confidence using non-maxima suppression."""
+    frameHeight = frame.shape[0]
+    frameWidth = frame.shape[1]
+
+    # Scan through all the bounding boxes output from the network and keep only the
+    # ones with high confidence scores. Assign the box's class label as the class with the highest score.
+    classIds = []
+    confidences = []
+    boxes = []
+
+    boxes_filtered = []
+    classIds_filtered = []
+    confidences_filtered = []
+
+    # Loop through all outputs.
+    for out in outs:
+        for detection in out:
+            if detection[4] > objectnessThreshold:
+                scores = detection[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+                if confidence > confThreshold:
+                    center_x = int(detection[0] * frameWidth)
+                    center_y = int(detection[1] * frameHeight)
+                    width = int(detection[2] * frameWidth)
+                    height = int(detection[3] * frameHeight)
+
+                    left = int(center_x - width / 2)
+                    top = int(center_y - height / 2)
+                    classIds.append(classId)
+                    confidences.append(float(confidence))
+                    boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+    for i in indices:
+        box = boxes[i]
+        boxes_filtered.append(box)
+        classIds_filtered.append(classIds[i])
+        confidences_filtered.append(confidences[i])
+        if debug:
+            left = box[0]
+            top = box[1]
+            width = box[2]
+            height = box[3]
+            cv2.rectangle(frame, (left, top), (left + width, top + height), (255, 255, 255), 2)
+        # label = "{}:{:.2f}".format(classes[classIds[i]], confidences[i])
+        # display_text(frame, label, left, top)
+    return boxes_filtered, classIds_filtered, confidences_filtered
+
+
+def getOutputsNames(net):
+    """Get the names of all output layers in the network."""
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i - 1] for i in net.getUnconnectedOutLayers()]
--- license_plate_detection.py
+++ license_plate_detection.py
@@ -4,7 +4,57 @@ import glob
 import matplotlib.pyplot as plt
 import pytesseract
 import Utilities
-from Utilities import load_images_from_folder
+from Utilities import load_images_from_folder, getOutputsNames, find_bounding_boxes
+
+debug = True
+
+def car_image_detection(network, image, width, height):
+    # set neural network input
+    blob = cv2.dnn.blobFromImage(image, 1 / 255, (width, height), [0, 0, 0], 1, crop=False)
+    network.setInput(blob)
+    # retrieve neural network outputs and find bounding boxes
+    outs = network.forward(getOutputsNames(network))
+    box_image = image.copy()
+    boxes, classIds, confidences = find_bounding_boxes(box_image, outs)
+
+    if debug:
+        cv2.namedWindow("bounding box car", cv2.WINDOW_NORMAL)
+        cv2.imshow("bounding box car", box_image)
+        cv2.waitKey(0)
+
+    car_images = []
+    for car_box in boxes:
+        left,top,car_width,car_height = car_box
+        car_image = image[top:top+car_height, left:left+car_width]
+        car_images.append(car_image)
+
+    return car_images
+
+'''
+Funtions that returns the set of license plates found in a set of images. The function is composed of 2 steps:
+1) In the first step the function detect the set of cars in an image
+2) Given the cars found in the first step, the function detects the respective license plates
+
+'''
+def detect_license_plates(images, vehicle_detection, plate_detection):
+    NN_width = 416
+    NN_height = 416
+    for image in images:
+
+        #Car detection
+        detected_cars = car_image_detection(vehicle_detection, image, NN_height, NN_height)
+
+        for detected_car in detected_cars:
+            if debug:
+                cv2.namedWindow("bounding box car", cv2.WINDOW_NORMAL)
+                cv2.imshow("bounding box car", detected_car)
+                cv2.waitKey(0)
+
+
+
+
+
+
 
 #load input images
 input_images = load_images_from_folder("Images/")
@@ -18,5 +68,9 @@ veic_detection_net = cv2.dnn.readNetFromDarknet(config_file_veic_detection, mode
 config_file_plate_detection = 'yolov3config_plate_detection.cfg'
 model_file_plate_detection = 'yolov3_plate_detection.weights'
 license_plate_detection_net = cv2.dnn.readNetFromDarknet(config_file_plate_detection, model_file_plate_detection)
+
+detect_license_plates(input_images, veic_detection_net, license_plate_detection_net)
+
+
 
 
